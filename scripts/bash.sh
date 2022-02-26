9f0f78140ec602b74e5f238920bbf33227ed1406
kristen-schneider <kristen.schneider@colorado.edu>
2022-02-26 15:12:27 -0700

    new main

--- scripts/bash.sh
+++ scripts/bash.sh
@@ -2,7 +2,9 @@
 
 data_file='../data/col1.tsv'
 block_size=1000
+data_type=1
+codec='gzip'
 
-echo "Running column 1. integers."
-python integers.py $data_file $block_size
+echo "Running program..."
+python main.py $data_file $block_size $data_type $codec
 
--- scripts/main.py
+++ scripts/main.py
@@ -1,2 +1,22 @@
-in_data = ''
-block_size = 3
+import sys
+import integers
+import floats
+import strings
+import compress_gzip
+
+data_file = sys.argv[1]
+block_size = sys.argv[2]
+data_type = sys.argv[3]
+codec = sys.argv[4]
+
+def main():
+    if data_type == 1:
+        print("Integers...")
+        blocks = integers.make_blocks(data_file)
+    elif data_type == 2:
+        blocks = floats.make_blocks(data_file)
+    elif data_type == 3:
+        blocks = strings.make_blocks(data_file)
+    else:
+        print('This data type is not supported. RIP.\n')
+        return -1
