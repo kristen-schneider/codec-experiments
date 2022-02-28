5f4c6cebce81b21b7da2133158b16050a429f3d3
Kristen Schneider <krsc0813@mendel.colorado.edu>
2022-02-28 11:35:06 -0700

    renamed bash.sh to run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+data_file='/home/krsc0813/projects/gwas-compress/gwas_files/columns/'
+#data_file='../data/col1.tsv'
+block_size=10000
+codec='gzip'
+
+echo "Running program..."
+echo "BZ2"
+python main.py $data_file/col1.tsv $block_size 1 bz2
+#python main.py $data_file $block_size 3 bz2
+#
+#echo "GZIP"
+#python main.py $data_file $block_size 1 gzip
+#python main.py $data_file $block_size 3 gzip
+#
+#echo "ZLIB"
+#python main.py $data_file $block_size 1 zlib
+#python main.py $data_file $block_size 3 zlib
+
+
+
+
