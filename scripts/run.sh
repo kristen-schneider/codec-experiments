a85763826394294c294b7ca222eb0eef76c0bd09
Kristen Schneider <krsc0813@mendel.colorado.edu>
2022-02-28 12:10:25 -0700

    x and y working

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,15 +7,15 @@ block_size=10000
 echo "Running program..."
 echo "BZ2"
 python main.py $data_file $block_size 1 bz2
-#python main.py $data_file $block_size 3 bz2
-#
-#echo "GZIP"
-#python main.py $data_file $block_size 1 gzip
-#python main.py $data_file $block_size 3 gzip
-#
-#echo "ZLIB"
-#python main.py $data_file $block_size 1 zlib
-#python main.py $data_file $block_size 3 zlib
+python main.py $data_file $block_size 3 bz2
+
+echo "GZIP"
+python main.py $data_file $block_size 1 gzip
+python main.py $data_file $block_size 3 gzip
+
+echo "ZLIB"
+python main.py $data_file $block_size 1 zlib
+python main.py $data_file $block_size 3 zlib
 
 
 
