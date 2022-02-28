1e35fe52bb36e789ad74ec7e12368668b18ba478
kristen-schneider <kristen.schneider@colorado.edu>
2022-02-28 16:57:30 -0700

    run.sh strucgture same onmendel ;

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-data_dir="/home/krsc0813/projects/gwas-compress/gwas_files/columns/"
-#data_dir="../data/"
+#data_dir="/home/krsc0813/projects/gwas-compress/gwas_files/columns/"
+data_dir="../data/"
 bash_dir="./bash_column/"
-block_size=10000
+block_size=3
 
 echo "Running experiments..."
 for col_bash in `ls $bash_dir`
