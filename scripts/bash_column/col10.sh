97255809b517947a333ecaefbc63acc3bad3ed54
kristen-schneider <kristen.schneider@colorado.edu>
2022-02-28 21:44:00 -0700

    only running offset for 5-10

--- scripts/bash_column/col10.sh
+++ scripts/bash_column/col10.sh
@@ -7,30 +7,40 @@ col="COL10"
 
 # COL10
 echo "BZ2 for $col"
-python main.py $data_file $block_size 1 bz2 s
-python main.py $data_file $block_size 2 bz2 s
-python main.py $data_file $block_size 3 bz2 s
+#python main.py $data_file $block_size 1 bz2 s
+python main.py $data_file $block_size 1 bz2 s o
+#python main.py $data_file $block_size 2 bz2 s
+python main.py $data_file $block_size 2 bz2 s o
+#python main.py $data_file $block_size 3 bz2 s
 
 echo "GZIP for $col"
-python main.py $data_file $block_size 1 gzip s
-python main.py $data_file $block_size 2 gzip s
-python main.py $data_file $block_size 3 gzip s
+#python main.py $data_file $block_size 1 gzip s
+python main.py $data_file $block_size 1 gzip s o
+#python main.py $data_file $block_size 2 gzip s
+python main.py $data_file $block_size 2 gzip s o
+#python main.py $data_file $block_size 3 gzip s
 
 echo "ZLIB for $col"
-python main.py $data_file $block_size 1 zlib s
-python main.py $data_file $block_size 2 zlib s
-python main.py $data_file $block_size 3 zlib s
+#python main.py $data_file $block_size 1 zlib s
+python main.py $data_file $block_size 1 zlib s o
+#python main.py $data_file $block_size 2 zlib s
+python main.py $data_file $block_size 2 zlib s o
+#python main.py $data_file $block_size 3 zlib s
 
 echo "PYFAST for $col"
-python main.py $data_file $block_size 1 fastpfor128 n
+#python main.py $data_file $block_size 1 fastpfor128 n
+python main.py $data_file $block_size 1 fastpfor128 n o
 
 echo "FPZIP for $col"
-python main.py $data_file $block_size 2 fpzip n
+#python main.py $data_file $block_size 2 fpzip n
+python main.py $data_file $block_size 2 fpzip n o
 
 echo "ZFPY for $col"
 #python main.py $data_file $block_size 1 zfpy n
-python main.py $data_file $block_size 2 zfpy n
+#python main.py $data_file $block_size 2 zfpy n
+python main.py $data_file $block_size 2 zfpy n o
 
 echo "PYZFP for $col"
 #python main.py $data_file $block_size 1 pyzfp n
-python main.py $data_file $block_size 2 pyzfp n
+#python main.py $data_file $block_size 2 pyzfp n
+python main.py $data_file $block_size 2 pyzfp n o
--- scripts/bash_column/col8.sh
+++ scripts/bash_column/col8.sh
@@ -7,24 +7,30 @@ col="COL8"
 
 # COL8
 echo "BZ2 for $col"
-python main.py $data_file $block_size 2 bz2 s
-python main.py $data_file $block_size 3 bz2 s
+#python main.py $data_file $block_size 2 bz2 s
+python main.py $data_file $block_size 2 bz2 s o
+#python main.py $data_file $block_size 3 bz2 s
 
 echo "GZIP for $col"
-python main.py $data_file $block_size 2 gzip s
-python main.py $data_file $block_size 3 gzip s
+#python main.py $data_file $block_size 2 gzip s
+python main.py $data_file $block_size 2 gzip s o
+#python main.py $data_file $block_size 3 gzip s
 
 echo "ZLIB for $col"
-python main.py $data_file $block_size 2 zlib s
-python main.py $data_file $block_size 3 zlib s
+#python main.py $data_file $block_size 2 zlib s
+python main.py $data_file $block_size 2 zlib s o
+#python main.py $data_file $block_size 3 zlib s
 
 echo "FPZIP for $col"
-python main.py $data_file $block_size 2 fpzip n
+#python main.py $data_file $block_size 2 fpzip n
+python main.py $data_file $block_size 2 fpzip n o
 
 echo "ZFPY for $col"
 #python main.py $data_file $block_size 1 zfpy n
-python main.py $data_file $block_size 2 zfpy n
+#python main.py $data_file $block_size 2 zfpy n
+python main.py $data_file $block_size 2 zfpy n o
 
 echo "PYZFP for $col"
 #python main.py $data_file $block_size 1 pyzfp n
-python main.py $data_file $block_size 2 pyzfp n
+#python main.py $data_file $block_size 2 pyzfp n
+python main.py $data_file $block_size 2 pyzfp n o
