e1c9d1403d240ea9f9be6df31bd091b49863b564
kristen-schneider <kristen.schneider@colorado.edu>
2022-02-28 21:04:09 -0700

    only col1 offset

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-#data_dir="/home/krsc0813/projects/gwas-compress/gwas_files/columns/"
-data_dir="../data/"
+data_dir="/home/krsc0813/projects/gwas-compress/gwas_files/columns/"
+#data_dir="../data/"
 bash_dir="./bash_column/"
 block_size=10000
 
 echo "Running experiments..."
-for col_bash in `ls $bash_dir`
-do
-  echo $col_bash
-  bash $bash_dir$col_bash $block_size $data_dir
-done
+bash $bash_dir"col1.sh" $block_size $data_dir
+#for col_bash in `ls $bash_dir`
+#do
+#  echo $col_bash
+#  bash $bash_dir$col_bash $block_size $data_dir
+#done
 
 
 
